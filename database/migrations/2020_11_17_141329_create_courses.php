7c8d0527a37f49ea01628ad2769ae6b28810b606
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-17 21:29:47 +0700

    create migrations

--- database/migrations/2020_11_17_141329_create_courses.php
+++ database/migrations/2020_11_17_141329_create_courses.php
@@ -15,6 +15,15 @@ class CreateCourses extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('certificate');
+            $table->string('thumbnail')->nullable();
+            $table->enum('type', ['free', 'premium']);
+            $table->enum('status', ['draft', 'published']);
+            $table->integer('price')->default(0)->nullable();
+            $table->enum('level', ['all-level', 'beginner', 'intermediate', 'advance']);
+            $table->longText('description')->nullable();
+            $table->foreignId('mentor_id')->constrained('mentors')->onDelete('cascade');
             $table->timestamps();
         });
     }
